52e535689fa7d77544133b07662d0e3ae52bbbe9
Ashish Kumar saw <ashishkumarsaw3@gmail.com>
2019-11-11 22:28:35 +0530

    Create permuteString.py

---
+++ permuteString.py
@@ -0,0 +1,24 @@
+def permute(s, curr, a = 0):
+
+    for i in range(a):
+        print(" ", end=" ")
+    if len(s) != 0:
+        print("permute("+"".join(s)+", "+"".join(curr)+", "+str(a)+"):")
+    else:
+        print("permute("+"".join(s)+", "+"".join(curr)+", "+str(a)+"):", end=" ")
+
+    if len(s) == 0:
+        print("".join(curr))
+    else:
+        for i in range(len(s)):
+            c = s[i]
+            curr.append(c)
+            s.pop(i)
+            permute(s, curr, a+1)
+
+            s.insert(i, c)
+            curr.pop()
+
+s = ['A','B','C']
+
+permute(s, [])
